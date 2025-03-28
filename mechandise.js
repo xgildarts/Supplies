79df507f4c27ec747ea510d3970c5dc9b15f4b77
xgildarts <xnatsu25@gmail.com>
2025-03-28 21:38:59 +0800

    v2 - in progress

---
+++ mechandise.js
@@ -0,0 +1,6 @@
+
+const backBtn = document.querySelector(".fa-chevron-left");
+
+backBtn.addEventListener("click", function(e) {
+    window.location.href = "selections.html";
+});
---
+++ uniform.js
@@ -0,0 +1,7 @@
+
+const backBtn = document.querySelector(".fa-chevron");
+
+
+backBtn.addEventListener("click", function(e) {
+    window.location.href = "index.html";
+});
