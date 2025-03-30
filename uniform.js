78f19ab17790cbf912809b7469b1db521b8af66b
xgildarts <xnatsu25@gmail.com>
2025-03-31 05:09:53 +0800

    auto generate added and revenue channel, total

--- uniform.js
+++ uniform.js
@@ -1,19 +1,29 @@
 
 const backBtn = document.querySelector(".fa-chevron-left");
 const form = document.querySelector("form");
-const product_name = document.querySelector(".product_name");
+
+let name = document.querySelector(".name");
+let product_name = document.querySelector(".product_name");
+let receipt_number = document.querySelector(".receipt_number");
 let price = document.querySelector(".price");
 let sku = document.querySelector(".sku");
 let size = document.querySelector(".size");
+let quantity = document.querySelector(".quantity");
+let date = document.querySelector(".date");
+let total = document.querySelector(".total");
+let revenue_channel = document.querySelector(".revenue_channel");
+
 
 document.addEventListener("DOMContentLoaded", (e) => {
 
     getUniformName();
     getSize();
+    revenueChannel();
     autoGeneratePriceAndSKU();
 
 });
 
+//Set back button
 if(backBtn) {
     backBtn.addEventListener("click", function(e) {
         const result = window.confirm('Are you sure do you want to go back?');
@@ -25,8 +35,71 @@ if(backBtn) {
     console.log("Cannot read the class");
 }
 
+//Set total
+quantity.addEventListener("input", function(e) {
+    total.value = parseInt(price.value) * parseInt(quantity.value);
+});
+
+//Validate quantity input
+quantity.addEventListener("input", function(e) {
+    if(quantity.value <= 0) {
+        quantity.value = 1;
+    }
+});
+
+
 //Sending a data to google sheet
 form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    let product_n = "";
+    let newDate = date.value.split("-");
+    newDate = `${newDate[1]}/${newDate[2]}/${newDate[0]}`;
+
+    if(size.value == "NONE") {
+        product_n = product_name.value
+    } else {
+        product_n = `${product_name.value} ${size.value}`
+    }
+
+    let payload = {
+
+        request_name: "insert",
+        date: newDate,
+        sku: sku.value,
+        product_name: product_n,
+        revenue_channel: revenue_channel.value,
+        quantity: quantity.value,
+        price: price.value,
+        receipt_number: receipt_number.value,
+        name: name.value
+
+    };
+
+    fetch("http://localhost/Supplies/uniform_api.php", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+    .then((response) => response.json())
+    .then((val) => {
+        window.alert(val[0]);
+    })
+    .catch((err) => console.error(err));
+
+    // date.value = null;
+    // sku.value = null;
+    // product_name.value = "Choose a product";
+    // size.value = "Choose a size";
+    // revenue_channel.value = "Choose revenue channel";     Here we  stop
+    // quantity.value = 1;
+    // price.value = 0;
+    // receipt_number.value = 0;
+    // name.value = null;
+    // total.value = 0;
     
 });
 
@@ -98,6 +171,11 @@ function autoGeneratePriceAndSKU() {
                 if(element[1] == `${product_name.value} ${size.value}`) {
                     sku.value = element[0];
                     price.value = element[2];
+                } else if(size.value == 'NONE') {
+                    if(element[1] == `${product_name.value}`) {
+                        sku.value = element[0];
+                        price.value = element[2];
+                    }
                 }
             });
         });
@@ -109,6 +187,11 @@ function autoGeneratePriceAndSKU() {
                 if(element[1] == `${product_name.value} ${size.value}`) {
                     sku.value = element[0];
                     price.value = element[2];
+                } else if(size.value == 'NONE') {
+                    if(element[1] == `${product_name.value}`) {
+                        sku.value = element[0];
+                        price.value = element[2];
+                    }
                 }
             });
         });
@@ -118,3 +201,27 @@ function autoGeneratePriceAndSKU() {
 
 }
 
+
+//Get all revenue channel
+function revenueChannel() {
+
+    fetch("http://localhost/Supplies/uniform_api.php", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({request_name: "revenue_channel"})
+    })
+    .then((response) => response.json())
+    .then((val) => {
+        let htmlCode = '<option selected disabled>Choose revenue channel</option>';
+        val.forEach(element => {
+            htmlCode += `<option value='${element[0]}'>${element[0]}</option>`;
+        });
+        revenue_channel.innerHTML = htmlCode;
+
+    })
+    .catch((err) => console.error(err));
+
+
+}
--- uniform_api.php
+++ uniform_api.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: POST, GET");
 
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $package = file_get_contents("php://input");
@@ -12,18 +13,42 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if($contents["request_name"] == "uniforms") {
 
-        $uniforms = file_get_contents('https://script.google.com/macros/s/AKfycbwjzk6QOxxt6PQcRpJChRX0LscKddSu74eortHECktDD8aaja_Y0sOVGBMIR0PBCCqg/exec?action=uniforms');
+        $uniforms = file_get_contents('https://script.google.com/macros/s/AKfycbzcHUH14SQ9XzXdBQj03MmGZw7zj6QrS9n-3RFie_2UOC1DFTGVAxhEiYxXzFoX_C0WPg/exec?action=uniforms');
         echo $uniforms;
 
     } else if($contents["request_name"] == "sizes") {
 
-        $uniforms = file_get_contents('https://script.google.com/macros/s/AKfycbwjzk6QOxxt6PQcRpJChRX0LscKddSu74eortHECktDD8aaja_Y0sOVGBMIR0PBCCqg/exec?action=sizes');
-        echo $uniforms; 
+        $sizes = file_get_contents('https://script.google.com/macros/s/AKfycbzcHUH14SQ9XzXdBQj03MmGZw7zj6QrS9n-3RFie_2UOC1DFTGVAxhEiYxXzFoX_C0WPg/exec?action=sizes');
+        echo $sizes; 
         
     } else if($contents["request_name"] == "comparing") {
 
-        $uniforms = file_get_contents('https://script.google.com/macros/s/AKfycbwjzk6QOxxt6PQcRpJChRX0LscKddSu74eortHECktDD8aaja_Y0sOVGBMIR0PBCCqg/exec?action=comparing');
-        echo $uniforms; 
+        $items = file_get_contents('https://script.google.com/macros/s/AKfycbzcHUH14SQ9XzXdBQj03MmGZw7zj6QrS9n-3RFie_2UOC1DFTGVAxhEiYxXzFoX_C0WPg/exec?action=comparing');
+        echo $items; 
+
+    } else if($contents["request_name"] == "insert") {
+
+        $url = "https://script.google.com/macros/s/AKfycbz2OwJ9JzyJ0MNf4Fj0p3JoOzFwwsY6CqtEiq6wnMydFmgbidLDZNFrjd88qzN0OhzgvA/exec";
+
+        $options = [
+            "http" => [
+                "method" => "POST",
+                "header" => "Content-Type: application/json",
+                "content" => $package,
+                "ignore_errors" => true
+            ]
+        ];
+
+        $context = stream_context_create($options);
+
+        $response = file_get_contents($url, false, $context);
+
+        echo json_encode(["Succesfully insert new data!"]);
+
+    } else if($contents["request_name"] == "revenue_channel") {
+
+        $revenue_channel = file_get_contents('https://script.google.com/macros/s/AKfycbzcHUH14SQ9XzXdBQj03MmGZw7zj6QrS9n-3RFie_2UOC1DFTGVAxhEiYxXzFoX_C0WPg/exec?action=revenue_channel');
+        echo $revenue_channel; 
 
     }
 
